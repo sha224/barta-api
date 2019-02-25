3a077373945aa5be4782fc61732b16896a510f3c
Shakhar Dasgupta <shakhardasgupta@gmail.com>
2019-02-24 23:26:24 -0500

    Add post search by location and distance

--- src/main/java/app/barta/api/PostRepository.java
+++ src/main/java/app/barta/api/PostRepository.java
@@ -1,7 +1,11 @@
 package app.barta.api;
 
+import java.util.List;
+
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	List<Post> findByLocationNear(Point location, Distance distance);
 }
